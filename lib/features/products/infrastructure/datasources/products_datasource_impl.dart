4ec8cf965ab8f75e3f6f5ace674c897484b51a0a
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-07-03 01:44:13 +0100

    feat: send backend on backend

--- lib/features/products/infrastructure/datasources/products_datasource_impl.dart
+++ lib/features/products/infrastructure/datasources/products_datasource_impl.dart
@@ -17,17 +17,49 @@ class ProductsDatasourceImpl extends ProudctsDatasources {
           }),
         );
 
-  @override
-  Future<ProductEntity> createUpdateProduct( Map<String, dynamic> productLike) async {
+  Future<String> _uploadFile(String path) async {
+    try {
+      final fileName = path.split('/').last;
 
+      final FormData data = FormData.fromMap(
+          {'file': MultipartFile.fromFileSync(path, filename: fileName)});
+
+      final response = await dio.post('/files/product', data: data);
+
+      return response.data['image'];
+    } catch (e) {
+      throw Exception();
+    }
+  }
+
+  Future<List<String>> _uploadPhotos(List<String> photos) async {
+    final photosToUpload =
+        photos.where((element) => element.contains('/')).toList();
+
+    final photosToIgnore =
+        photos.where((element) => !element.contains('/')).toList();
+
+    final List<Future<String>> uploadJob =
+        photosToUpload.map(_uploadFile).toList();
+
+    final newImages = await Future.wait(uploadJob);
+
+    return [...photosToIgnore, ...newImages];
+  }
+
+  @override
+  Future<ProductEntity> createUpdateProduct(
+      Map<String, dynamic> productLike) async {
     try {
       final String? productId = productLike['id'];
 
       final String method = (productId == null) ? 'POST' : 'PATCH';
 
-      final String url = (productId == null) ? '/products' : '/products/$productId';
+      final String url =
+          (productId == null) ? '/products' : '/products/$productId';
 
       productLike.remove('id');
+      productLike['images'] = await _uploadPhotos(productLike['images']);
 
       final response = await dio.request(
         url,
@@ -41,7 +73,6 @@ class ProductsDatasourceImpl extends ProudctsDatasources {
     } catch (e) {
       throw Exception(e.toString());
     }
-    
   }
 
   @override
